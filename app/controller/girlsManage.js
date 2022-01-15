08936e55ff34ea80f4679a15e9a4e4ee93cd7ffd
foxscallion <foxscallion@163.com>
2022-01-15 20:43:42 +0800

    update egg content

--- app/controller/girlsManage.js
+++ app/controller/girlsManage.js
@@ -5,20 +5,44 @@ const Controller = require("egg").Controller;
 class GirlManage extends Controller {
   async addGirl() {
     const { ctx } = this;
-
-    ctx.body = "添加用户";
+    const params = {
+      name: '小白',
+      age: 18,
+      skill: 'web'
+    }
+    const res = await ctx.service.testdb.addGirl(params);
+    console.log(res,'res');
+    ctx.body = res;
   }
 
   async delGirl() {
     const { ctx } = this;
-
-    ctx.body = "删除用户";
+    const params = {
+      id: '1000'
+    }
+    const res = await ctx.service.testdb.delGirl(params);
+    if(res) {
+      ctx.body = '删除用户成功';
+    }else {
+      ctx.body = '删除用户失败';
+    }
   }
 
   async updateGirl() {
     const { ctx } = this;
-
-    ctx.body = "修改用户";
+    const params = {
+      name: '李四',
+      age: 20,
+      skill: 'java',
+      id: 1001
+    }
+    const res = await ctx.service.testdb.updateGirl(params);
+    console.log(res,'res');
+    if(res) {
+      ctx.body = '修改用户成功';
+    }else {
+      ctx.body = '修改用户失败';
+    }
   }
 
   async getGirls() {
--- app/service/testdb.js
+++ app/service/testdb.js
@@ -4,24 +4,47 @@ const Service = require("egg").Service;
 
 class testdbService extends Service {
     // 添加数据库
-    async addGirl() {}
+    async addGirl(params) {
+        try {
+            const {app} = this;
+            const res = await app.mysql.insert('girls',params);
+            return res;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
     // 删除数据库
-    async delGirl() {}
+    async delGirl(id) {
+        try {
+            const {app} = this;
+            const res = app.mysql.delete('girls',id);
+            return res;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
     // 修改数据库
-    async updateGirl() {}
-
-    // 查询数据库
+    async updateGirl(params) {
+        try {
+            const {app} = this;
+            const res = app.mysql.update('girls',params);
+            return res;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
     // 查询数据库
     async getGirl() {
         try {
             const app = this.app;
-            const res = app.mysql.select('t_user');
-            // await res.query(sql, values);
+            const res = app.mysql.select('girls');
             return res
-
         } catch (error) {
             console.log(error)
-            return '无数据'
+            return null
         }
     }
 }
